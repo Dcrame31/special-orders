4dac5419a6d37b80fc2b270020fb6193d78443ec
Diane Crame <dcrame@icloud.com>
2021-05-15 19:30:24 -0700

    order form

--- src/components/customer/customerForm.js
+++ src/components/customer/customerForm.js
@@ -1,14 +1,50 @@
 import React from 'react';
 
-class customerForm extends Component {
-    render() {
+const CustomerForm = () => {
+   
         return (
-            <>  
-                
-            </>
+            <div>  
+                <h1>Cookbook Order Form</h1>
+                <form className="ui form">
+                    <div className="field">
+                        <label>Customer Name </label>
+                        <input type="text"/>
+                    </div>
+                    <div className="field">
+                        <label>Phone Number</label>
+                        <input type="number"/>
+                    </div>
+                    <div className="field">
+                        <label>Email Address</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="field">
+                        <label>Product</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="field">
+                        <label>Pickup Time</label>
+                        <input type="number"/>
+                    </div>
+                    <div className="field">
+                        <label>Credit Card Number</label>
+                        <input type="number"/>
+                    </div>
+                    <div className="field">
+                        <label>Location</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="field">
+                        <label>Order Taken By</label>
+                        <input type="text"/>
+                    </div>
+                    <button type="submit" className="ui button">Submit</button>
+
+                </form>
+            </div>
         )
-    }
+    
     
 }
 
-export default customerForm;
+export default CustomerForm;
